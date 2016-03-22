6569510e86c11cfb9bbd3a9089c4bf64e87552b9
Chad Blomquist <chad.blomquist@gmail.com>
2016-03-22 01:04:01 -0700

    Finish Lesson 2

---
+++ app/src/main/java/com/example/android/sunshine/app/ForecastFragment.java
@@ -0,0 +1,199 @@
+package com.example.android.sunshine.app;
+
+import android.os.AsyncTask;
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.text.format.Time;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class ForecastFragment extends Fragment {
+
+    public ArrayAdapter<Forecast> m_forecastAdapter;
+    private static final String LOG_TAG = "ForecastFragment";
+
+    public ForecastFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+
+        m_forecastAdapter = new ArrayAdapter<>(getActivity(), R.layout.list_item_forecast,
+                R.id.list_item_forecast_textview, new ArrayList<Forecast>());
+        ListView listView = (ListView) rootView.findViewById(R.id.listview_forecast);
+        listView.setAdapter(m_forecastAdapter);
+
+        setHasOptionsMenu(true);
+
+        return rootView;
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        inflater.inflate(R.menu.forecast_fragment, menu);
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.action_refresh) {
+            Toast.makeText(getContext(), "Refreshing forecast data.", Toast.LENGTH_SHORT).show();
+            FetchWeatherTask weatherTask = new FetchWeatherTask();
+            weatherTask.execute(new ForecastRequest("98001,USA","metric","json",7));
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
+
+    class FetchWeatherTask extends AsyncTask<ForecastRequest, Integer, List<Forecast>> {
+
+        private static final String s_apiKey = "d3ece1824d6807e32666e075d3b95e9f";
+        private static final String LOG_TAG = "FetchWeatherTask";
+
+        @Override
+        protected void onPostExecute(List<Forecast> forecasts) {
+            super.onPostExecute(forecasts);
+            m_forecastAdapter.clear();
+            for (Forecast f : forecasts) {
+                m_forecastAdapter.add(f);
+            }
+        }
+
+        @Override
+        protected List<Forecast> doInBackground(ForecastRequest... params) {
+            if (params== null || params.length == 0) {
+                return null;
+            }
+            ForecastRequest req = params[0];
+            HttpURLConnection urlConnection = null;
+            BufferedReader reader = null;
+
+            String forecastJsonStr = null;
+
+            try {
+                URL url = new URL(req.getUri(s_apiKey).toString());
+
+                urlConnection = (HttpURLConnection) url.openConnection();
+                urlConnection.setRequestMethod("GET");
+                urlConnection.connect();
+
+                InputStream inputStream = urlConnection.getInputStream();
+                StringBuffer buffer = new StringBuffer();
+                if (inputStream == null) {
+                    return null;
+                }
+                reader = new BufferedReader(new InputStreamReader(inputStream));
+
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    buffer.append(line + "\n");
+                }
+
+                if (buffer.length() == 0) {
+                    return null;
+                }
+                forecastJsonStr = buffer.toString();
+            } catch (IOException e) {
+                Log.e(LOG_TAG, "Error ", e);
+                return null;
+            }
+            finally{
+                if (urlConnection != null) {
+                    urlConnection.disconnect();
+                }
+                if (reader != null) {
+                    try {
+                        reader.close();
+                    } catch (final IOException e) {
+                        Log.e(LOG_TAG, "Error closing stream", e);
+                    }
+                }
+            }
+            try {
+                return getWeatherDataFromJson(forecastJsonStr);
+            } catch (JSONException e) {
+                Log.e(LOG_TAG, "doInBackground: ", e);
+                return null;
+            }
+        }
+
+        private String getReadableDateString(long time){
+            SimpleDateFormat shortenedDateFormat = new SimpleDateFormat("EEE MMM dd");
+            return shortenedDateFormat.format(time);
+        }
+
+        private List<Forecast> getWeatherDataFromJson(String forecastJsonStr)
+                throws JSONException {
+
+            List<Forecast> forecasts = new ArrayList<>();
+
+            final String OWM_LIST = "list";
+            final String OWM_WEATHER = "weather";
+            final String OWM_TEMPERATURE = "temp";
+            final String OWM_MAX = "max";
+            final String OWM_MIN = "min";
+            final String OWM_DESCRIPTION = "main";
+
+            JSONObject forecastJson = new JSONObject(forecastJsonStr);
+            JSONArray weatherArray = forecastJson.getJSONArray(OWM_LIST);
+
+            Time dayTime = new Time();
+            dayTime.setToNow();
+
+            int julianStartDay = Time.getJulianDay(System.currentTimeMillis(), dayTime.gmtoff);
+
+            dayTime = new Time();
+
+            for(int i = 0; i < weatherArray.length(); i++) {
+                Forecast forecast = new Forecast();
+                JSONObject dayForecast = weatherArray.getJSONObject(i);
+
+                long dateTime;
+                dateTime = dayTime.setJulianDay(julianStartDay+i);
+                forecast.setDateString(getReadableDateString(dateTime));
+
+                JSONObject weatherObject = dayForecast.getJSONArray(OWM_WEATHER).getJSONObject(0);
+                forecast.setDescription(weatherObject.getString(OWM_DESCRIPTION));
+
+                JSONObject temperatureObject = dayForecast.getJSONObject(OWM_TEMPERATURE);
+                forecast.setHigh(temperatureObject.getDouble(OWM_MAX));
+                forecast.setLow(temperatureObject.getDouble(OWM_MIN));
+                forecasts.add(forecast);
+            }
+            return forecasts;
+        }
+    }
+
+}
---
+++ app/src/main/java/com/example/android/sunshine/app/ForecastRequest.java
@@ -0,0 +1,72 @@
+package com.example.android.sunshine.app;
+
+import android.net.Uri;
+
+public class ForecastRequest {
+    private static final String s_scheme = "http";
+    private static final String s_authority = "api.openweathermap.org";
+    private static final String[] s_path = {"data","2.5","forecast","daily"};
+
+    private String m_city;
+    private String m_units;
+    private String m_mode;
+    private int m_count;
+
+    public ForecastRequest(String city, String units, String mode, int count) {
+        m_city = city;
+        m_units = units;
+        m_mode = mode;
+        m_count = count;
+    }
+
+    public String getCity() {
+        return m_city;
+    }
+
+    public void setCity(String city) {
+        this.m_city = city;
+    }
+
+    public String getUnits() {
+        return m_units;
+    }
+
+    public void setUnits(String units) {
+        this.m_units = units;
+    }
+
+    public String getMode() {
+        return m_mode;
+    }
+
+    public void setMode(String mode) {
+        this.m_mode = mode;
+    }
+
+    public int getCount() {
+        return m_count;
+    }
+
+    public void setCount(int count) {
+        this.m_count = count;
+    }
+
+    public Uri getUri(String appid) {
+        Uri.Builder builder = new Uri.Builder();
+
+        builder.scheme(s_scheme)
+            .authority(s_authority);
+        for (String s : s_path) {
+            builder.appendPath(s);
+        }
+        builder.appendQueryParameter("q", m_city)
+                .appendQueryParameter("mode", m_mode)
+                .appendQueryParameter("units", m_units)
+                .appendQueryParameter("count", ""+m_count)
+                .appendQueryParameter("appid", appid);
+
+
+        return builder.build();
+    }
+
+}
