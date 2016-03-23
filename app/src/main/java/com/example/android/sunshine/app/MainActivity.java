4166affc65435ae7a0f627420bd929839d10f3b1
Chad Blomquist <chad.blomquist@gmail.com>
2016-03-22 19:32:09 -0700

    Finish lesson 4a.

--- app/src/main/java/com/example/android/sunshine/app/MainActivity.java
+++ app/src/main/java/com/example/android/sunshine/app/MainActivity.java
@@ -22,7 +22,40 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-   }
+        Log.v(LOG_TAG, "RotateTest -- onCreate");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.v(LOG_TAG, "RotateTest -- onPause");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.v(LOG_TAG, "RotateTest -- onResume");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.v(LOG_TAG, "RotateTest -- onStop");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.v(LOG_TAG, "RotateTest -- onStart");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.v(LOG_TAG, "RotateTest -- onDestroy");
+    }
+
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
